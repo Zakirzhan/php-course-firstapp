4b227a51418a36b0cb291f7178d9eed4c5b49735
Zakirzhan <zako1996@mail.ru>
2020-02-12 00:31:13 +0100

    phph2

--- app/core.php
+++ app/core.php
@@ -6,10 +6,7 @@ require_once('config.php');
 //добавляем класс базы данных
 require_once(APP_DIRECTORY.'app/models/MyDB.class.php');
 $mydb = new MyDB($db);	
-
-//добавляем routes
-require_once(APP_DIRECTORY.'app/models/Routes.class.php');
-
+ 
 //добавляем класс сессии
 require_once(APP_DIRECTORY.'app/models/Session.class.php');
 Session::start();
